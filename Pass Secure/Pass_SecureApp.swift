5643644316ccc5407925c86ed1349bc85b24ec13
orpheus lau <orpheuslau@gmail.com>
2024-02-29 10:55:41 +0800

    Initial Commit

---
+++ Pass Secure/Pass_SecureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pass_SecureApp.swift
+//  Pass Secure
+//
+//  Created by Omon 3 on 29/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pass_SecureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
